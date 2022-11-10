92d7f901bc875d277b3466599039554814def8a4
akiyomov <114338623+akiyomov@users.noreply.github.com>
2022-11-10 22:37:24 +0900

    permissons added

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Patient
 from .serializers import PatientSerializer
 # Create your views here.
 class PatientAPIView(ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
