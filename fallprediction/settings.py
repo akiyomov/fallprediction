7adef7532ac8f8d4583513f959baa8fa25414afe
Jahongir <jakhongiralikhanov@gmail.com>
2022-11-24 12:07:30 +0500

    update

--- fallprediction/settings.py
+++ fallprediction/settings.py
@@ -80,10 +80,12 @@ MIDDLEWARE = [
 ]
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost",
+    "http://127.0.0.1:3000"
+]
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'fallprediction.urls'
